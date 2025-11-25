5b9956a790ecb6c883aae414e77e7ff469421719
Daniel L <daniel.lee202@education.nsw.gov.au>
2025-11-26 09:13:07 +1100

    A

---
+++ jira_belt/imu_globals.h
@@ -0,0 +1,5 @@
+#ifndef IMU_GLOBALS_H
+#define IMU_GLOBALS_H
+#include "LSM6DS3.h"
+extern IMU_LSM6DS3 imu;
+#endif
--- jira_belt/jira_belt.ino
+++ jira_belt/jira_belt.ino
@@ -1,7 +1,11 @@
 #include <Wire.h>
 #include "Seeed_vl53l0x.h"
+#include <LSM6DS3.h>
+#include "imu_globals.h"
+IMU_LSM6DS3 imu(I2C_MODE, 0x6A);
 
 Seeed_vl53l0x VL53L0X;
+
 
 // Alert thresholds (mm)
 const int criticalDistance = 100;   // < 10cm
@@ -9,17 +13,11 @@ const int warningDistance = 300;    // < 30cm
 const int safeDistance = 500;       // > 50cm
 
 const int USPin = 2;
-const int TOFPin = 3;
 const int AGPin = 4;
 const int speakerPin = 5;
 const int motorPin = 6;  
 const int VibrationPin = 7;  
 
-
-#include <Wire.h>
-#include "LSM6DS3.h"
-
-IMU_LSM6DS3 imu(I2C_MODE, 0x6A);  // I2C address 0x6A
 
 void setup() {
 speaker_setup();

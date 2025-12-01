c5fc2a8a2f9bbcb298505f3179ac4082e4e32ad2
Bryan H <bryan.huang1@education.nsw.gov.au>
2025-12-01 11:51:10 +1100

    e

--- jira_belt/us_sensor.ino
+++ jira_belt/us_sensor.ino
@@ -1,9 +1,27 @@
-void us_sensor_setup() {
+#include "Ultrasonic.h"
 
+Ultrasonic ultrasonic(5);
+const int alertDistance = 20;  // Alert if object within 20cm
+const int ledPin = 13;  // Built-in LED
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
-void us_sensor_loop() {
+void loop() {
+  long distance = ultrasonic.MeasureInCentimeters();
 
-  
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance > 0 && distance < alertDistance) {
+    digitalWrite(ledPin, HIGH);  // Turn on LED if object is close
+    Serial.println("WARNING: Object detected!");
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(250);
 }

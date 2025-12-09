3303aaae024a5f30926977d4cc08c293739c5188
Daniel L <daniel.lee202@education.nsw.gov.au>
2025-12-09 22:14:24 +0000

    Test

--- jira_belt/jira_belt.ino
+++ jira_belt/jira_belt.ino
@@ -8,38 +8,34 @@ LSM6DS3 imu(I2C_MODE, 0x6A);
 Seeed_vl53l0x VL53L0X;
 
 // Alert thresholds (mm)
-const int criticalDistance = 100;   // < 10cm // TOF
-const int warningDistance = 300;    // < 30cm // TOF
-const int safeDistance = 500;       // > 50cm // TIF
-const int alertDistance = 20;  // Alert if object within 20cm // US
+const int criticalDistance = 100;  // < 10cm // TOF
+const int warningDistance = 300;   // < 30cm // TOF
+const int safeDistance = 500;      // > 50cm // TIF
+const int alertDistance = 20;      // Alert if object within 20cm // US
 
 const int USPin = 2;
 const int AGPin = 4;
 const int speakerPin = 8;
-const int vibrationPin = 6;  
+const int vibrationPin = 6;
 
 Ultrasonic ultrasonic(5);
 
 void setup() {
   Serial.begin(9600);
-  vibration_motor_setup();
-  //speaker_setup();
-  //us_setup();
-  //tof_setup();
-
+  // vibration_motor_setup();
+  speaker_setup();
+  us_setup();
+  tof_setup();
 }
 
 void loop() {
-  vibration_motor_loop();
-  /* tof_loop(); 
+  // vibration_motor_loop();
+  tof_loop();
   us_loop();
-
 
 
   if (g_tof_distance <= 1000 & g_tof_distance > 2) {
     Serial.println("WARNING");
     speaker_loop();
+  }
 }
-*/
-}
- 
